f48da32f96b070f6be2a9d9b7063d3197a5853b4
Prashansa <prashansabharti19@gmail.com>
2024-05-25 17:42:26 +0530

    frontend website nae update

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ require("dotenv").config();
 
 // app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://deliciosdispatch.netlify.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With , Content-Type ,Accept "
